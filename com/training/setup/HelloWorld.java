1cace4064600aaf7499f8487f5d89cc7042c1afc
Kavya Sri <kavyasri607@gmail.com>
2024-02-08 11:29:53 +0530

    Added a main method

--- com/training/setup/HelloWorld.java
+++ com/training/setup/HelloWorld.java
@@ -1,7 +1,12 @@
-package  com.training.setup;
+package com.training.setup;
 
 public class HelloWorld {
 
-// add methods
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
+
+//add, push, commit, pull.
+//init, fetch, merge , rebase, stash, upstream, downstream, .gitignore
